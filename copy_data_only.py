0b5bc30a0dd75b45324741ede142c1abeb175f09
rmarabini <roberto@cnb.csic.es>
2018-05-01 22:19:38 +0200

    refactored, create a copyFiles class and create API to paramiko

--- copy_data_only.py
+++ copy_data_only.py
@@ -4,14 +4,13 @@ import sys
 
 if __name__ == '__main__':
     description = 'Copy Data (no project) files'
-    epilog = 'Example: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/' % \
+    epilog = 'Example: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare' % \
              __file__
     epilog += '\nExample: %s 2018_project_blas jmv@hilbert:.' % __file__
-    epilog += '\nExample: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/ ' \
+    epilog += '\nExample: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare ' \
               '--timeout 1' % __file__
     epilog += '\n  where 1 is run for 1 day (5 days is the default)'
-    epilog += '\nUMOUNT (as administrator): sudo umount /volumeUSB2/usbshare/' \
-              '2018_04_16_belen_t7)'
+    epilog += '\nUMOUNT (as administrator): sudo umount /volumeUSB2/usbshare'
     projectName, target, timeout = _usage(description, epilog)
     copyfile = CopyFiles(projectName, target, timeout)
     exitcode =copyfile._copy_files([EPUDATADIR], timeout)
