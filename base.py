0518ea0e3fedb90448a71870b3f80d97aa41295b
rmarabini <roberto@cnb.csic.es>
2018-05-01 13:35:15 +0200

    refractored, now a list of datatypes is pased to copy. In this way the last script copy projects and data is easier to handle

--- base.py
+++ base.py
@@ -8,26 +8,35 @@ from interruptingcow import timeout
 import glob
 from argparse import RawTextHelpFormatter
 
-DIR='/volume1/homes/scipionuser/'
-DATADIR='OffloadData/'
+DATADIR= '/volume1/homes/scipionuser/'
+EPUDATADIR= 'OffloadData/'
 PROJECTDIR='Projects/'
 RSYNC='/usr/bin/rsync'
 
 def _usage(description, epilog):
-    """ Print usage information"""
+    """ Print usage information and process command line
+        returns: project name
+                 target directory
+                 timeout (stop) seconds
+    """
     projectDir = '/var/services/homes/scipionuser/Projects/20*'
     for name in glob.glob(projectDir):
         print("  ", os.path.basename(name))
 
-    parser = argparse.ArgumentParser(description=description, formatter_class=RawTextHelpFormatter, epilog=epilog)
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=RawTextHelpFormatter,
+                                     epilog=epilog)
     parser.add_argument("projname", help="source directory")
     parser.add_argument("target", help="target directory")
-    parser.add_argument("--timeout", help="timeout (default=432000, 5 days)", default=432000)
+    parser.add_argument("--timeout", help="timeout (default=5, unit=days)",
+                        default=5)
     args = parser.parse_args()
-    return args.projname, args.target, args.timeout
+    return args.projname, args.target, args.timeout * 24 * 60 * 60 # convert to seconds
 
 def _remote(target):
-    """ return  username hosr dir from target"""
+    """ return  username, host and target dir by parsing target
+                if local copy username = host = None
+    """
     if "@" in target:
         parse = target.split(":")[0].split("@")
         return parse[0], parse[1], target.split(":")[1]
@@ -37,7 +46,9 @@ def _remote(target):
 
 
 def _createDirectory(projectName, targetDir, username=None, host=None):
-    """ Create directory either local or remote"""
+    """ Create directory either local or remote
+        data will be copied to  this directory
+    """
     dir = os.path.join(targetDir, projectName)
 
     if username is None: #local rsync
@@ -50,37 +61,44 @@ def _createDirectory(projectName, targetDir, username=None, host=None):
         ssh.connect(host, 22, username, None)
         ssh.exec_command('mkdir -p ' + dir)
         ssh.close
-
+"""
 copy_files typeData = list
 for data in list
    argsList,append(args
 
 inside while
      for ars in arsList:
+"""
 
-def _copy_files(projectName, typeData, targetDir, username, host, _timeout):
+def _copy_files(projectName, typeDataList, targetDir, username, host, _timeout):
     """loop that copies files"""
-    args = [RSYNC]
-    args.append("-va")
-    args.append("--progress")
-    source = os.path.join(DIR, typeData, projectName + "/")
-    args.append(source)
-    targetDir = os.path.join(targetDir, projectName, typeData)
-    if username is not None:
-        args.append("%s@%s:%s" % (username, host, targetDir))
-    else:
-        args.append(targetDir)
+    argsList={} # disctionary with directories to be copied
+    for  typeData in typeDataList:
+        args = [RSYNC]
+        args.append("-va")
+        args.append("--progress")
+        source = os.path.join(DATADIR, typeData, projectName + "/")
+        args.append(source)
+        targetDir = os.path.join(targetDir, projectName, typeData)
+        if username is not None:
+            args.append("%s@%s:%s" % (username, host, targetDir))
+        else:
+            args.append(targetDir)
+        argsList[typeData] = args
+
+
 
     try:
         with timeout(_timeout, exception=RuntimeError): # _timeout seconds
             while True:
-                rsyncproc = subprocess.Popen(args)
+                for typeData in typeDataList:
+                    rsyncproc = subprocess.Popen(argsList[typeData])
                 print "sleeping";sys.stdout.flush()
                 time.sleep(900)
                 print "weaking up";sys.stdout.flush()
 
     except RuntimeError:
-        print "didn't finish within %d seconds" % _timeout
+        print "Aborting, copy didn't finish within %d seconds" % _timeout
 
     exitcode = rsyncproc.wait()
     return exitcode
--- copy_project_only.py
+++ copy_project_only.py
@@ -1,18 +1,26 @@
 #!/volume1/@appstore/python/bin/python
-from base import _copy_files, _createDirectory, _remote, _usage, DATADIR, PROJECTDIR
+from base import _copy_files, _createDirectory, _remote, _usage, EPUDATADIR, PROJECTDIR
 import sys
 
 if __name__ == '__main__':
     description = 'Copy Project (no movies) files'
-    epilog = 'Example: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/.' % __file__
+    epilog = 'Example: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/.' % \
+             __file__
     epilog += '\nExample: %s 2018_project_blas jmv@hilbert:.' % __file__
-    epilog += '\nExample: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/. --timeout 1' % __file__
+    epilog += '\nExample: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/. ' \
+              '--timeout 1' % __file__
     epilog += '\n  where 1 is run for 1 day (5 days is the default)'
-    epilog += '\nUMOUNT (as administrator): sudo umount /volumeUSB2/usbshare/2018_04_16_belen_t7)'
+    epilog += '\nUMOUNT (as administrator): sudo umount /volumeUSB2/usbshare' \
+              '/2018_04_16_belen_t7)'
     projectName, target, timeout = _usage(description, epilog)
     username, host, targetDir = _remote(target)
     _createDirectory(projectName, targetDir, username, host)
-    exitcode = _copy_files(projectName, PROJECTDIR, targetDir, username, host, timeout)
+    exitcode = _copy_files(projectName,
+                           [PROJECTDIR],  # data type to be transfered
+                           targetDir,
+                           username,
+                           host,
+                           timeout)
 
     if exitcode==0:
         pass
