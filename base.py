934fa9169eb064818e9d6e09ef17b2eb8b7f4ca6
Your Name <you@example.com>
2018-05-01 13:04:47 +0200

    data_only and project_only done

--- base.py
+++ base.py
@@ -8,8 +8,11 @@ from interruptingcow import timeout
 import glob
 from argparse import RawTextHelpFormatter
 
-DATADIR='/volume1/homes/scipionuser/OffloadData/'
+DIR='/volume1/homes/scipionuser/'
+DATADIR='OffloadData/'
+PROJECTDIR='Projects/'
 RSYNC='/usr/bin/rsync'
+
 def _usage(description, epilog):
     """ Print usage information"""
     projectDir = '/var/services/homes/scipionuser/Projects/20*'
@@ -24,7 +27,7 @@ def _usage(description, epilog):
     return args.projname, args.target, args.timeout
 
 def _remote(target):
-    """ return tru is target has the symbol @ (remote rsync)"""
+    """ return  username hosr dir from target"""
     if "@" in target:
         parse = target.split(":")[0].split("@")
         return parse[0], parse[1], target.split(":")[1]
@@ -32,9 +35,10 @@ def _remote(target):
         return None, None, target
 
 
+
 def _createDirectory(projectName, targetDir, username=None, host=None):
     """ Create directory either local or remote"""
-    dir = targetDir # os.path.join(targetDir, projectName)
+    dir = os.path.join(targetDir, projectName)
 
     if username is None: #local rsync
         if not os.path.exists(dir):
@@ -47,13 +51,21 @@ def _createDirectory(projectName, targetDir, username=None, host=None):
         ssh.exec_command('mkdir -p ' + dir)
         ssh.close
 
-def _copy_files(projectName, targetDir, username, host, _timeout):
+copy_files typeData = list
+for data in list
+   argsList,append(args
+
+inside while
+     for ars in arsList:
+
+def _copy_files(projectName, typeData, targetDir, username, host, _timeout):
     """loop that copies files"""
     args = [RSYNC]
     args.append("-va")
     args.append("--progress")
-    source = os.path.join(DATADIR, projectName)
+    source = os.path.join(DIR, typeData, projectName + "/")
     args.append(source)
+    targetDir = os.path.join(targetDir, projectName, typeData)
     if username is not None:
         args.append("%s@%s:%s" % (username, host, targetDir))
     else:
@@ -62,7 +74,6 @@ def _copy_files(projectName, targetDir, username, host, _timeout):
     try:
         with timeout(_timeout, exception=RuntimeError): # _timeout seconds
             while True:
-                print "executing " + ' '.join(args);sys.stdout.flush()
                 rsyncproc = subprocess.Popen(args)
                 print "sleeping";sys.stdout.flush()
                 time.sleep(900)
--- copy_project_only.py
+++ copy_project_only.py
@@ -1,19 +1,18 @@
 #!/volume1/@appstore/python/bin/python
-from base import _copy_files, _createDirectory, _remote, _usage
+from base import _copy_files, _createDirectory, _remote, _usage, DATADIR, PROJECTDIR
 import sys
 
 if __name__ == '__main__':
-    description = 'Copy Data (no project) files'
+    description = 'Copy Project (no movies) files'
     epilog = 'Example: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/.' % __file__
     epilog += '\nExample: %s 2018_project_blas jmv@hilbert:.' % __file__
     epilog += '\nExample: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare/. --timeout 1' % __file__
     epilog += '\n  where 1 is run for 1 day (5 days is the default)'
     epilog += '\nUMOUNT (as administrator): sudo umount /volumeUSB2/usbshare/2018_04_16_belen_t7)'
     projectName, target, timeout = _usage(description, epilog)
-    print projectName, target, timeout
     username, host, targetDir = _remote(target)
     _createDirectory(projectName, targetDir, username, host)
-    exitcode = _copy_files(projectName, targetDir, username, host, timeout)
+    exitcode = _copy_files(projectName, PROJECTDIR, targetDir, username, host, timeout)
 
     if exitcode==0:
         pass
