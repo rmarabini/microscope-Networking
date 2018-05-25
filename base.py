1ca9a6e924b1694194bf020bc3f0cc26f838de37
rmarabini <roberto@cnb.csic.es>
2018-05-25 17:09:09 +0200

    print number of transferred files

--- base.py
+++ base.py
@@ -7,17 +7,28 @@ from interruptingcow import timeout
 import glob
 from argparse import RawTextHelpFormatter
 import select
+import fnmatch
 
-DATADIR= '/volume1/homes/scipionuser/'
-EPUDATADIR= 'OffloadData/'
-PROJECTDIR='Projects/'
-RSYNC='/usr/bin/rsync'
+DATADIR = '/volume1/homes/scipionuser/'
+EPUDATADIR = 'OffloadData/'
+PROJECTDIR = 'Projects/'
+RSYNC = '/usr/bin/rsync'
 SCIPIONHOST = 'scipionbox'
 RUSKAHOST = 'ruska'
 SCIPIONUSER = 'scipionuser'
 SCIPIONDATADIR = '/home/%s/ScipionUserData' % SCIPIONUSER
-LOGS='Logs'
-SLEEPTIME=3600 # seconds
+LOGS ='Logs'
+SLEEPTIME = 3600 # seconds
+
+def locate(pattern, root_path, skipPattern=None):
+    """ Return recursive list of files in directory root_path
+        that match pattern pattern
+    """
+    for path, dirs, files in os.walk(os.path.abspath(root_path)):
+        for filename in fnmatch.filter(files, pattern):
+            if skipPattern is not None and filename.endswith(skipPattern):
+                continue
+            yield os.path.join(path, filename)
 
 def _usage(description, epilog):
     """ Print usage information and process command line
@@ -28,11 +39,11 @@ def _usage(description, epilog):
     # Print directory information
     print "PROJECT NAMES------------------------------"
     projectDir = '/var/services/homes/scipionuser/Projects/20*'
-    for name in glob.glob(projectDir):
+    for name in sorted(glob.glob(projectDir)):
         print "  ", os.path.basename(name)
     print "USB DISK AVAILABLES------------------------"
     usbDir = '/volumeUSB*/usbshare'
-    for name in glob.glob(usbDir):
+    for name in sorted(glob.glob(usbDir)):
         print "  ", name
 
     parser = argparse.ArgumentParser(description=description,
@@ -43,10 +54,13 @@ def _usage(description, epilog):
     parser.add_argument("--timeout", help="timeout (default=5, unit=days)",
                         default=5., type=float)
     args = parser.parse_args()
-    return args.projname, args.target, int(args.timeout * 24 * 60 * 60) # convert days to seconds
+    timeout = int(args.timeout * 24 * 60 * 60) # convert days to seconds
+
+    return args.projname, args.target, timeout
+
 
 class RemoteCommands:
-    "class to execute a multiple commands in a remote host"
+    "class to execute  multiple commands in a remote host"
     def __init__(self, retry_time=0):
         self.retry_time = retry_time
 
@@ -95,7 +109,9 @@ class RemoteCommands:
 
 
 class CopyFiles():
-
+    """ Copy files generated in either Scipion projects or
+        OffloadData (movies) directory
+    """
     def __init__(self, projectName, target, timeout):
         self.projectName = projectName
         self.timeout = timeout
@@ -111,7 +127,7 @@ class CopyFiles():
             self.targetHost = None
             self.targetDir = target
             self.localTarget = True
-        self.remoteCommand = RemoteCommands(2) # retry comamnd 2 times
+        self.remoteCommand = RemoteCommands(2) # retry comand 2 times
 
     def _createDirectory(self, typeData):
         """ Create directory either local or remote
@@ -127,52 +143,89 @@ class CopyFiles():
                                        self.targetHost,
                                        ['mkdir -p %s' % dir])
 
+    def printNumberFiles(self, source, target, dataType, 
+                         pattern="*", localTarget = True,
+                         skipPattern=None):
+        source = source.strip()
+        target = target.strip()
+        print "----------------------------------"
+        print "INFO: There are %d files in SOURCE dir %s that match pattern = %s " % \
+             (len([js for js in locate(pattern, source, skipPattern)]),
+                     source, pattern)
+        if self.localTarget:
+             print "INFO: There are %d files in TARGET dir %s that match pattern = %s " % \
+                     (len([js for js in locate(pattern, target, skipPattern)]), target, pattern)
+        #else:
+        #     target = target.split(":")[1]
+        #     print 'INFO: Files in REMOTE target dir. Execute in remote host: find ~/%s -name "%s" |wc -l  \n\n REMEMBER that the number_of_files_in_the_target_project_dir = number_files_in_origin_dir - number_of_movies' % (target, pattern)
+        #print "Note: During data acquisition the number of files in Source dir will be greater than files in Target dir"
+        #if skipPattern is not None:
+        #    print "Note: files ending in %s have not been counted since there are links" % skipPattern
+        print "----------------------------------"
+
     def _copy_files(self, typeDataList, _timeout):
         """loop that copies files"""
 
+        logFileName = os.path.join(DATADIR,LOGS,self.projectName)
         if  EPUDATADIR in typeDataList:
             typeData = EPUDATADIR
             self._createDirectory(typeData)
+            sourceDirEPU = os.path.join(DATADIR, typeData, self.projectName + "/ ")
+            targetDirEPU = os.path.join(self.targetDir, self.projectName, typeData)
+            if not self.localTarget:
+                targetDirEPU = "%s@%s:%s" % (self.targetUserName, self.targetHost, targetDirEPU)
+
             cmdEPU = RSYNC + \
                   " -vrlt" + \
                   " --progress " + \
-                  " --log-file=%s " % os.path.join(DATADIR,LOGS,self.projectName)  + \
-                  os.path.join(DATADIR, typeData, self.projectName + "/ ")
-            targetDir = os.path.join(self.targetDir, self.projectName, typeData)
-            if self.localTarget:
-                cmdEPU += targetDir
-            else:
-                cmdEPU += "%s@%s:%s" % (self.targetUserName, self.targetHost, targetDir)
+                  " --log-file=%s " % logFileName  + \
+                  sourceDirEPU + \
+                  targetDirEPU
 
         if PROJECTDIR in typeDataList:
             typeData = PROJECTDIR
             self._createDirectory(typeData)
+            sourceDirProj = os.path.join(DATADIR, typeData, self.projectName + "/ ")
+            targetDirProj = os.path.join(self.targetDir, self.projectName, typeData)
+            if not self.localTarget:
+                targetDirProj = "%s@%s:%s" % (self.targetUserName, self.targetHost, targetDirProj)
             cmdProj = RSYNC + \
                   " -vrlt" + \
                   " --progress --delete " + \
                   ' --exclude="*Fractions.mrc" ' + \
-                  " --log-file=%s " % os.path.join(DATADIR,LOGS,self.projectName)  + \
-                  os.path.join(DATADIR, typeData, self.projectName + "/ ")
-            targetDir = os.path.join(self.targetDir, self.projectName, typeData)
-            if self.localTarget:
-                cmdProj += targetDir
-            else:
-                cmdProj += "%s@%s:%s" % (self.targetUserName, self.targetHost, targetDir)
+                  " --log-file=%s " % logFileName  + \
+                  sourceDirProj + \
+                  targetDirProj
 
         try:
             with timeout(_timeout, exception=RuntimeError): # _timeout seconds
                 while True:
                     if EPUDATADIR in typeDataList:
+                        typeData = EPUDATADIR
                         print cmdEPU
                         os.system(cmdEPU)
                     if PROJECTDIR in typeDataList:
+                        typeData = EPUDATADIR
                         print cmdProj
                         os.system(cmdProj)
-                        #self.remoteCommand.run_cmd(SCIPIONHOST, [cmdProj])
+                    if EPUDATADIR in typeDataList:
+                        self.printNumberFiles(sourceDirEPU,
+                                              targetDirEPU, 
+                                              typeData, 
+                                              pattern="*Fractions.mrc",
+                                              localTarget = self.localTarget)
+                    if PROJECTDIR in typeDataList:    
+                        self.printNumberFiles(sourceDirProj,
+                                              targetDirProj,                             
+                                              typeData,                                  
+                                              pattern="*",
+                                              localTarget=self.localTarget,
+                                              skipPattern="Fractions.mrc")
                     print "sleeping";sys.stdout.flush()
                     time.sleep(SLEEPTIME)
                     print "weaking up";sys.stdout.flush()
-
+                    if self.localTarget:
+                        pass
         except RuntimeError:
             print "Aborting, copy didn't finish within %d seconds" % _timeout
 
