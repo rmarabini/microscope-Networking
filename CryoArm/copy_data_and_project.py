5ab0efe2c755b2a6d88f4d0410ed1ef41f811162
Your Name <you@example.com>
2020-07-29 14:12:53 +0200

    update copy scripts for talos

---
+++ CryoArm/copy_data_and_project.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from base import CopyFiles, _usage, EPUDATADIR, PROJECTDIR
+import sys
+
+if __name__ == '__main__':
+    description = 'Copy Data AND Project  files'
+    epilog = 'Example: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare' % \
+             __file__
+    epilog += '\nExample: %s 2018_project_blas jmv@hilbert:.' % __file__
+    epilog += '\nExample: %s 2018_04_16_belen_t7 /volumeUSB2/usbshare ' \
+              '--timeout 1' % __file__
+    epilog += '\n  where 1 is run for 1 day (5 days is the default)'
+    epilog += '\nUMOUNT (as administrator): sudo umount /volumeUSB2/usbshare'
+
+    projectName, target, timeout = _usage(description, epilog)
+    copyfile = CopyFiles(projectName, target, timeout)
+    exitcode =copyfile._copy_files([EPUDATADIR, PROJECTDIR], timeout)
+
+    if exitcode==0:
+        pass
+    else:
+        print("\nWARNING: looks like some error occured :( \n\n")
+        sys.exit(1)
+
+    print ("Done")
